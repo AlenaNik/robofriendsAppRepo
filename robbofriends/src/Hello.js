19b5139bdc421033a119dae4f619a6b960fdfe6a
AlenaNik <alena.nik.digital@gmail.com>
2019-04-24 08:24:40 +0200

    component

--- robbofriends/src/Hello.js
+++ robbofriends/src/Hello.js
@@ -4,7 +4,8 @@ class Hello extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Hello World</h1>
+                <p>Welcome to React</p>
             </div>
         );
     }
